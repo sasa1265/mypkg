f48cb0aef59210f755a9b72cf1c4457bb2a3a262
sasa1265 <mostafamahmoud01005376205@gmail.com>
2025-04-11 23:43:30 +0200

    Update mypkg.go

--- mypkg.go
+++ mypkg.go
@@ -1,16 +1,12 @@
 package main
 
 import (
+    "fmt"
     "os"
-    "os/exec"
-    "log"
 )
 
 func main() {
-    cmd := exec.Command("whoami")  // مثال لأمر غير مرغوب فيه، يمكنك استبداله بما يناسب
-    out, err := cmd.CombinedOutput()
-    if err != nil {
-        log.Fatal(err)
-    }
-    log.Println(string(out))
+    // استخدام مكتبة os لقراءة متغير بيئة
+    user := os.Getenv("USER")
+    fmt.Println("User:", user)
 }
