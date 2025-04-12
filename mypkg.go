212077f00af6c2ab4b2a0085d97e7d6cbdf47175
sasa1265 <mostafamahmoud01005376205@gmail.com>
2025-04-12 03:33:25 +0200

    Update mypkg.go

--- mypkg.go
+++ mypkg.go
@@ -1,7 +1,23 @@
-import (
-    "os"
-    "html/template"
-    "net/http"
-    "os/exec"
-    "regexp"
-)
+package main
+
+import "os/exec"
+
+func main() {
+  // Command example
+  cmd := exec.Command("echo", "hello")
+
+  // CommandContext example
+  err := exec.CommandContext(ctx, "sleep", "5").Run()
+
+  // Not vulnerable
+  cmd := exec.Command("echo", "1; cat /etc/passwd")
+
+  // Vunerable
+  userInput :=  "echo 1 | cat /etc/passwd" // value supplied by user input
+  out, _ = exec.Command("sh", "-c", userInput).Output()
+
+  // Vulnerable
+  userInput1 := "cat" // value supplied by user input
+  userInput2 := "/etc/passwd" // value supplied by user input
+  out, _ = exec.Command(userInput1, userInput2)
+}
