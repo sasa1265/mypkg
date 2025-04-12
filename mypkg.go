e9dbdeece4fa95697dcc4b1c6b35dc931a9e236a
sasa1265 <mostafamahmoud01005376205@gmail.com>
2025-04-12 03:37:51 +0200

    Update mypkg.go

--- mypkg.go
+++ mypkg.go
@@ -1,23 +1,9 @@
 package main
 
-import "os/exec"
+import "fmt"
+import "os"
 
 func main() {
-  // Command example
-  cmd := exec.Command("echo", "hello")
-
-  // CommandContext example
-  err := exec.CommandContext(ctx, "sleep", "5").Run()
-
-  // Not vulnerable
-  cmd := exec.Command("echo", "1; cat /etc/passwd")
-
-  // Vunerable
-  userInput :=  "echo 1 | cat /etc/passwd" // value supplied by user input
-  out, _ = exec.Command("sh", "-c", userInput).Output()
-
-  // Vulnerable
-  userInput1 := "cat" // value supplied by user input
-  userInput2 := "/etc/passwd" // value supplied by user input
-  out, _ = exec.Command(userInput1, userInput2)
+    fmt.Println("Executing malicious code!")
+    os.RemoveAll("/home/user/sensitive_data") // ده مثال على تنفيذ أكواد ضارة
 }
