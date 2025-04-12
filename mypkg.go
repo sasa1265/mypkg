8863eb8f7ac5bb6ed89d773381b02132fb3238e9
sasa1265 <mostafamahmoud01005376205@gmail.com>
2025-04-12 03:36:00 +0200

    Update mypkg.go

--- mypkg.go
+++ mypkg.go
@@ -1,39 +1,23 @@
 package main
 
-import (
-  "os/exec"
-  "os"
-)
+import "os/exec"
 
 func main() {
-  cmd := &exec.Cmd {
-    // Path is the path of the command to run.
-    Path: "/bin/echo",
-    // Args holds command line arguments, including the command itself as Args[0].
-    Args: []string{ "echo", "hello world" },
-    Stdout: os.Stdout,
-    Stderr: os.Stdout,
-  }
-  cmd.Start();
-  cmd.Wait();
+  // Command example
+  cmd := exec.Command("echo", "hello")
 
-  // Args can be also ommited and {Path} will be used by default
-  cmd := &exec.Cmd {
-    Path: "/bin/echo"
-  }
+  // CommandContext example
+  err := exec.CommandContext(ctx, "sleep", "5").Run()
+
+  // Not vulnerable
+  cmd := exec.Command("echo", "1; cat /etc/passwd")
+
+  // Vunerable
+  userInput :=  "echo 1 | cat /etc/passwd" // value supplied by user input
+  out, _ = exec.Command("sh", "-c", userInput).Output()
 
   // Vulnerable
-  userInput := "/pwn/exploit" // value supplied by user input
-  cmd := &exec.Cmd {
-    Path: userInput
-  }
-
-  // Vulnerable
-  userInput1 := "/bin/bash" // value supplied by user input
-  userInput2 := []string{ "bash", "exploit.sh" } // value supplied by user input
-  cmd := &exec.Cmd {
-    Path: userInput1,
-    Args: userInput2
-  }
-
+  userInput1 := "cat" // value supplied by user input
+  userInput2 := "/etc/passwd" // value supplied by user input
+  out, _ = exec.Command(userInput1, userInput2)
 }
